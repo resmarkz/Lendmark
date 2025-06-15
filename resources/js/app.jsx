7ca3a9ed621c6a14af91499bd29782fce4d89c5d
RESMARKZ <dacurawatmark@gmail.com>
2025-06-15 11:30:07 +0800

    feat: add breeze auth

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,18 +1,25 @@
-import "./bootstrap";
+import '../css/app.css';
+import './bootstrap';
 
-import { createInertiaApp } from "@inertiajs/react";
-import { createRoot } from "react-dom/client";
-import Layout from "./Layouts/Layout";
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    resolve: (name) => {
-        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
-        let page = pages[`./Pages/${name}.jsx`];
-        page.default.layout =
-            page.default.layout || ((page) => <Layout children={page} />);
-        return page;
-    },
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />);
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
     },
 });
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::inertia('/', 'Home/index')->name('home');
-
-Route::get('/page2', function () {
-    return Inertia::render('Page2/index', [
-        'message' => 'This is page 2',
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
-})->name('page2');
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
