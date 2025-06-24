dd772b31f1eb40319d48e2954e0b4138373d2bff
RESMARKZ <dacurawatmark@gmail.com>
2025-06-24 22:46:40 +0800

    ref: changed my department migration

--- database/migrations/2025_06_15_030009_create_departments_table.php
+++ database/migrations/2025_06_15_030009_create_departments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->enum("TELEMARKETER", "COLLECTIONS");
+            $table->enum('name', ['TELEMARKETER', 'COLLECTIONS']);
             $table->text('description')->nullable();
             $table->timestamps();
         });
